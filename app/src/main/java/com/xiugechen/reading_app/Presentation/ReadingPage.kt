0ce869526138a52369198c35ebbc18856ddd318a
Xiuge Chen <xiugec@student.unimelb.edu.au>
2019-12-29 21:12:57 +1100

    1.upgrade api min level to 26 to support justfy alignment. 2.change alignment to justify

--- app/src/main/java/com/xiugechen/reading_app/Presentation/ReadingPage.kt
+++ app/src/main/java/com/xiugechen/reading_app/Presentation/ReadingPage.kt
@@ -1,7 +1,9 @@
 package com.xiugechen.reading_app.Presentation
 
 import android.content.Intent
+import android.graphics.text.LineBreaker
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.xiugechen.reading_app.Data.DataManager
